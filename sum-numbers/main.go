60241f50897bd0ab67d81e09283f5f0981a2ea9d
Norbert Benczúr <norbert.benczur@gmail.com>
2019-04-27 20:12:45 +0200

    Optimized the code from n*n to n

--- sum-numbers/main.go
+++ sum-numbers/main.go
@@ -7,21 +7,12 @@ func main() {
 // IsAddUpTo Given a list of numbers and a number k,
 // returns whether any two numbers from the list add up to k.
 func IsAddUpTo(numbers []int, k int) bool {
-	count := len(numbers)
-	for i, number := range numbers {
-		if number == k {
+	differences := make(map[int]bool)
+	for _, number := range numbers {
+		if number == k || differences[number] {
 			return true
 		}
-		j := i
-		for {
-			j++
-			if j == count {
-				break
-			}
-			if number+numbers[j] == k {
-				return true
-			}
-		}
+		differences[k-number] = true
 	}
 	return false
 }
