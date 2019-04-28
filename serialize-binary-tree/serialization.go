8b4cec211c9d68b3b19389650e9b1fb3323f2330
Norbert Benczúr <norbert.benczur@gmail.com>
2019-04-28 15:48:46 +0200

    Solution for serialization problem

--- serialize-binary-tree/serialization.go
+++ serialize-binary-tree/serialization.go
@@ -1,5 +1,10 @@
 package serialization
 
+import (
+	"fmt"
+	"regexp"
+)
+
 type Node struct {
 	val   string
 	left  *Node
@@ -7,9 +12,40 @@ type Node struct {
 }
 
 func serialize(n *Node) string {
-	return ""
+	if n == nil {
+		return "nil"
+	}
+	return fmt.Sprintf("[ val: \"%s\", left: %s, right: %s ]", n.val, serialize(n.left), serialize(n.right))
 }
 
 func deserialize(s string) *Node {
-	return &Node{"", nil, nil}
+	if s == "" || s == "nil" {
+		return nil
+	}
+
+	r := regexp.MustCompile(`^\[ val: \"(\S*)\", left: nil, right: nil \]$`)
+	result := r.FindStringSubmatch(s)
+	if r.MatchString(s) {
+		return &Node{result[1], nil, nil}
+	}
+
+	r = regexp.MustCompile(`^\[ val: \"(\S*)\", left: \[(.*)\], right: nil \]$`)
+	result = r.FindStringSubmatch(s)
+	if r.MatchString(s) {
+		return &Node{result[1], deserialize(fmt.Sprintf("[%s]", result[2])), nil}
+	}
+
+	r = regexp.MustCompile(`^\[ val: \"(\S*)\", left: nil, right: \[(.*)\] \]$`)
+	result = r.FindStringSubmatch(s)
+	if r.MatchString(s) {
+		return &Node{result[1], nil, deserialize(fmt.Sprintf("[%s]", result[2]))}
+	}
+
+	r = regexp.MustCompile(`^\[ val: \"(\S*)\", left: \[(.*)\], right: \[(.*)\] \]$`)
+	result = r.FindStringSubmatch(s)
+	if r.MatchString(s) {
+		return &Node{result[1], deserialize(fmt.Sprintf("[%s]", result[2])), deserialize(fmt.Sprintf("[%s]", result[3]))}
+	}
+
+	return nil
 }
--- serialize-binary-tree/serialization_test.go
+++ serialize-binary-tree/serialization_test.go
@@ -6,6 +6,76 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestSerializeNil(t *testing.T) {
+	var node *Node
+	assert.Equal(t, "nil", serialize(node))
+}
+
+func TestDeserializeNil(t *testing.T) {
+	s := "nil"
+	var node *Node
+	assert.Equal(t, node, deserialize(s))
+}
+
+func TestSerializeEmptyNode(t *testing.T) {
+	node := &Node{}
+	assert.Equal(t, `[ val: "", left: nil, right: nil ]`, serialize(node))
+}
+
+func TestDeserializeEmptyNode(t *testing.T) {
+	assert.Equal(t, &Node{}, deserialize(`[ val: "", left: nil, right: nil ]`))
+}
+
+func TestSerializeNodeWithValue(t *testing.T) {
+	node := &Node{
+		val: "root",
+	}
+	assert.Equal(t, `[ val: "root", left: nil, right: nil ]`, serialize(node))
+}
+
+func TestDeserializeNodeWithValue(t *testing.T) {
+	assert.Equal(t, &Node{val: "root"}, deserialize(`[ val: "root", left: nil, right: nil ]`))
+}
+
+func TestSerializeLeftNode(t *testing.T) {
+	node := &Node{
+		left: &Node{val: "leftNode"},
+	}
+	assert.Equal(t, `[ val: "", left: [ val: "leftNode", left: nil, right: nil ], right: nil ]`, serialize(node))
+}
+
+func TestDeserializeLeftNode(t *testing.T) {
+	node := &Node{
+		left: &Node{val: "leftNode"},
+	}
+	s := `[ val: "", left: [ val: "leftNode", left: nil, right: nil ], right: nil ]`
+	assert.Equal(t, node, deserialize(s))
+}
+
+func TestSerializeRightNode(t *testing.T) {
+	node := &Node{
+		right: &Node{val: "rightNode"},
+	}
+	assert.Equal(t, `[ val: "", left: nil, right: [ val: "rightNode", left: nil, right: nil ] ]`, serialize(node))
+}
+
+func TestDeserializeRightNode(t *testing.T) {
+	node := &Node{
+		right: &Node{val: "rightNode"},
+	}
+	s := `[ val: "", left: nil, right: [ val: "rightNode", left: nil, right: nil ] ]`
+	assert.Equal(t, node, deserialize(s))
+}
+
+func TestDeserializeLeftAndRightNode(t *testing.T) {
+	node := &Node{
+		left:  &Node{val: "leftNode"},
+		right: &Node{val: "rightNode"},
+	}
+	s := `[ val: "", left: [ val: "leftNode", left: nil, right: nil ], right: [ val: "rightNode", left: nil, right: nil ] ]`
+	assert.Equal(t, node, deserialize(s))
+}
+
 func TestSerializeAndDeserializeBack(t *testing.T) {
 	node := &Node{"root", &Node{"left", &Node{"left.left", nil, nil}, nil}, &Node{"right", nil, nil}}
 	assert.Equal(t, "left.left", deserialize(serialize(node)).left.left.val)
