9bda8522925b1b91c8e32dacd6bd5fbcce05769d
Norbert Benczúr <norbert.benczur@gmail.com>
2019-05-10 16:50:06 +0200

    Added description to the func

--- job-scheduler/main.go
+++ job-scheduler/main.go
@@ -5,14 +5,14 @@ import (
 	"time"
 )
 
+// Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
 func schedule(f func(), n int) {
 	time.Sleep(time.Duration(n) * time.Millisecond)
 	f()
 }
 
-// Implement a job scheduler which takes in a function f and an integer n, and calls f after n milliseconds.
 func main() {
-	schedule(printHello, 1)
+	schedule(printHello, 2000)
 }
 
 func printHello() {
